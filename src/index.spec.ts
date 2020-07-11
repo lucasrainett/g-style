ae6481df1c652bf0d939d133dca5e4aba3dfa96e
Lucas Rainett <lucas@rainett.dev>
2020-07-11 17:21:17 +1200

    setup prettier

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,179 +1,171 @@
-import {GlobalStyle} from "./index";
+import { GlobalStyle } from "./index";
 
 describe("GlobalStyle", () => {
+	const insertRule = jest.fn();
 
-    const insertRule = jest.fn();
+	beforeEach(() => {
+		jest.resetAllMocks();
+		(document.head.appendChild as jest.Mock).mockReturnValue({
+			sheet: {
+				insertRule,
+				cssRules: []
+			}
+		});
+	});
 
-    beforeEach(() => {
-        jest.resetAllMocks();
-        (document.head.appendChild as jest.Mock).mockReturnValue({
-            sheet: {
-                insertRule,
-                cssRules: []
-            }
-        });
-    });
+	it("should create class name", () => {
+		const globalStyle = new GlobalStyle();
+		const classNames = globalStyle.getClassNames({ color: "gold" });
+		expect(classNames).toEqual(expect.any(String));
+	});
 
+	it("should insert style on document", () => {
+		const globalStyle = new GlobalStyle();
+		globalStyle.getClassNames({ color: "gold" });
+		expect(globalStyle.getFullCss()).toEqual(".t0{color:gold;}");
+	});
 
-    it("should create class name", () => {
-        const globalStyle = new GlobalStyle();
-        const classNames = globalStyle.getClassNames({color: "gold"});
-        expect(classNames).toEqual(expect.any(String));
-    });
+	it("should deduplicate rules", () => {
+		const globalStyle = new GlobalStyle();
+		const classNames1 = globalStyle.getClassNames({ color: "gold" });
+		const classNames2 = globalStyle.getClassNames({ color: "gold" });
 
-    it("should insert style on document", () => {
-        const globalStyle = new GlobalStyle();
-        globalStyle.getClassNames({color: "gold"});
-        expect(globalStyle.getFullCss()).toEqual(".t0{color:gold;}");
-    });
+		expect(classNames1).toEqual("t0");
+		expect(classNames2).toEqual("t0");
 
-    it("should deduplicate rules", () => {
-        const globalStyle = new GlobalStyle();
-        const classNames1 = globalStyle.getClassNames({color: "gold"});
-        const classNames2 = globalStyle.getClassNames({color: "gold"});
+		expect(globalStyle.getFullCss()).toEqual(".t0{color:gold;}");
+	});
 
-        expect(classNames1).toEqual("t0");
-        expect(classNames2).toEqual("t0");
+	it("should handles child selectors", () => {
+		const globalStyle = new GlobalStyle();
+		const className = globalStyle.getClassNames({
+			color: "gold",
+			"&:hover": {
+				color: "black"
+			}
+		});
 
-        expect(globalStyle.getFullCss()).toEqual(".t0{color:gold;}");
-    });
+		expect(className).toEqual("t0 t1");
+		expect(globalStyle.getFullCss()).toEqual(
+			[".t0{color:gold;}", ".t1:hover{color:black;}"].join("\n")
+		);
+	});
 
-    it("should handles child selectors", () => {
-        const globalStyle = new GlobalStyle();
-        const className = globalStyle.getClassNames({
-            color: "gold",
-            "&:hover": {
-                color: "black"
-            }
-        });
+	it("should handles media queries", () => {
+		const globalStyle = new GlobalStyle();
+		globalStyle.getClassNames({
+			color: "gold",
+			"@media print": {
+				color: "black"
+			}
+		});
+		expect(globalStyle.getFullCss()).toEqual(
+			[".t0{color:gold;}", "@media print{.t1{color:black;}}"].join("\n")
+		);
+	});
 
-        expect(className).toEqual("t0 t1");
-        expect(globalStyle.getFullCss()).toEqual([
-            ".t0{color:gold;}",
-            ".t1:hover{color:black;}"
-        ].join("\n"));
-    });
+	it("should handles media queries and child selectors", () => {
+		const globalStyle = new GlobalStyle();
+		globalStyle.getClassNames({
+			color: "gold",
+			"@media screen": {
+				"&:hover": {
+					color: "black"
+				}
+			}
+		});
+		expect(globalStyle.getFullCss()).toEqual(
+			[".t0{color:gold;}", "@media screen{.t1:hover{color:black;}}"].join("\n")
+		);
+	});
 
-    it("should handles media queries", () => {
-        const globalStyle = new GlobalStyle();
-        globalStyle.getClassNames({
-            color: "gold",
-            "@media print": {
-                color: "black"
-            }
-        })
-        expect(globalStyle.getFullCss()).toEqual([
-            ".t0{color:gold;}",
-            "@media print{.t1{color:black;}}"
-        ].join("\n"));
-    });
+	it("should ignore null values", () => {
+		const globalStyle = new GlobalStyle();
+		const classNames = globalStyle.getClassNames({
+			color: null
+		} as any);
+		expect(classNames).toEqual("");
+		expect(globalStyle.getFullCss()).toEqual("");
+	});
 
-    it("should handles media queries and child selectors", () => {
-        const globalStyle = new GlobalStyle();
-        globalStyle.getClassNames({
-            color: "gold",
-            "@media screen": {
-                "&:hover": {
-                    color: "black"
-                }
-            }
-        })
-        expect(globalStyle.getFullCss()).toEqual([
-            ".t0{color:gold;}",
-            "@media screen{.t1:hover{color:black;}}"
-        ].join("\n"));
-    });
+	it("should accept custom prefix", () => {
+		const globalStyle = new GlobalStyle("_cxs");
+		globalStyle.getClassNames({ color: "gold" });
+		expect(globalStyle.getFullCss()).toEqual("._cxs0{color:gold;}");
+	});
 
-    it("should ignore null values", () => {
-        const globalStyle = new GlobalStyle();
-        const classNames = globalStyle.getClassNames({
-            color: null
-        } as any)
-        expect(classNames).toEqual("");
-        expect(globalStyle.getFullCss()).toEqual("");
-    });
+	it("should handles sub class selectors", () => {
+		const globalStyle = new GlobalStyle();
+		const className = globalStyle.getClassNames({
+			color: "gold",
+			".button": {
+				color: "black"
+			}
+		});
 
-    it("should accept custom prefix", () => {
-        const globalStyle = new GlobalStyle("_cxs");
-        globalStyle.getClassNames({color: "gold"});
-        expect(globalStyle.getFullCss()).toEqual("._cxs0{color:gold;}");
-    });
+		expect(className).toEqual("t0 t1");
+		expect(globalStyle.getFullCss()).toEqual(
+			[".t0{color:gold;}", ".t1 .button{color:black;}"].join("\n")
+		);
+	});
 
-    it("should handles sub class selectors", () => {
-        const globalStyle = new GlobalStyle();
-        const className = globalStyle.getClassNames({
-            color: "gold",
-            ".button": {
-                color: "black"
-            }
-        });
+	it("should handles extension class selectors", () => {
+		const globalStyle = new GlobalStyle();
+		const className = globalStyle.getClassNames({
+			color: "gold",
+			"&.button": {
+				color: "black"
+			}
+		});
 
-        expect(className).toEqual("t0 t1");
-        expect(globalStyle.getFullCss()).toEqual([
-            ".t0{color:gold;}",
-            ".t1 .button{color:black;}"
-        ].join("\n"));
-    });
+		expect(className).toEqual("t0 t1");
+		expect(globalStyle.getFullCss()).toEqual(
+			[".t0{color:gold;}", ".t1.button{color:black;}"].join("\n")
+		);
+	});
 
-    it("should handles extension class selectors", () => {
-        const globalStyle = new GlobalStyle();
-        const className = globalStyle.getClassNames({
-            color: "gold",
-            "&.button": {
-                color: "black"
-            }
-        });
+	it("should support multiple instances", () => {
+		const globalStyle1 = new GlobalStyle("test1");
+		const globalStyle2 = new GlobalStyle("test2");
 
-        expect(className).toEqual("t0 t1");
-        expect(globalStyle.getFullCss()).toEqual([
-            ".t0{color:gold;}",
-            ".t1.button{color:black;}"
-        ].join("\n"));
-    });
+		const className1 = globalStyle1.getClassNames({
+			color: "gold"
+		});
 
-    it("should support multiple instances", () => {
-        const globalStyle1 = new GlobalStyle("test1");
-        const globalStyle2 = new GlobalStyle("test2");
+		const className2 = globalStyle2.getClassNames({
+			color: "gold"
+		});
 
-        const className1 = globalStyle1.getClassNames({
-            color: "gold"
-        });
+		expect(className1).toEqual("test10");
+		expect(className2).toEqual("test20");
 
-        const className2 = globalStyle2.getClassNames({
-            color: "gold"
-        });
+		expect(globalStyle1.getFullCss()).toEqual(".test10{color:gold;}");
+		expect(globalStyle2.getFullCss()).toEqual(".test20{color:gold;}");
+	});
 
-        expect(className1).toEqual("test10");
-        expect(className2).toEqual("test20");
+	it("should support complex styles", () => {
+		const globalStyle = new GlobalStyle();
+		const className = globalStyle.getClassNames({
+			color: "gold",
+			".box": {
+				backgroundColor: "red"
+			},
+			"&.button": {
+				color: "black"
+			},
+			"@media print": {
+				border: "1px solid #ccc"
+			}
+		});
 
-        expect(globalStyle1.getFullCss()).toEqual(".test10{color:gold;}");
-        expect(globalStyle2.getFullCss()).toEqual(".test20{color:gold;}");
-    });
-
-    it("should support complex styles", () => {
-        const globalStyle = new GlobalStyle();
-        const className = globalStyle.getClassNames({
-            color: "gold",
-            ".box": {
-                backgroundColor: "red"
-            },
-            "&.button": {
-                color: "black"
-            },
-            "@media print": {
-                border: "1px solid #ccc",
-            }
-        });
-
-        expect(className).toEqual("t0 t1 t2 t3");
-        expect(globalStyle.getFullCss()).toEqual([
-            ".t0{color:gold;}",
-            ".t1 .box{background-color:red;}",
-            ".t2.button{color:black;}",
-            "@media print{.t3{border:1px solid #ccc;}}"
-        ].join("\n"));
-    });
+		expect(className).toEqual("t0 t1 t2 t3");
+		expect(globalStyle.getFullCss()).toEqual(
+			[
+				".t0{color:gold;}",
+				".t1 .box{background-color:red;}",
+				".t2.button{color:black;}",
+				"@media print{.t3{border:1px solid #ccc;}}"
+			].join("\n")
+		);
+	});
 });
-
-
-
--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,68 @@
 export interface CssObject {
-    [key: string]: string | CssObject;
+	[key: string]: string | CssObject;
 }
 
-export class GlobalStyle{
-    private readonly cache: { [key: string]: string } = {};
-    private readonly rules: string[] = [];
-    private readonly sheet?: CSSStyleSheet;
+export class GlobalStyle {
+	private readonly cache: { [key: string]: string } = {};
+	private readonly rules: string[] = [];
+	private readonly sheet?: CSSStyleSheet;
 
-    constructor(private prefix: string = "t") {
-        if (typeof document !== "undefined") {
-            this.sheet = document.head.appendChild(document.createElement("style")).sheet as CSSStyleSheet;
-        }
-    }
+	constructor(private prefix: string = "t") {
+		if (typeof document !== "undefined") {
+			this.sheet = document.head.appendChild(document.createElement("style"))
+				.sheet as CSSStyleSheet;
+		}
+	}
 
-    private parseCssObject(style: CssObject, childSelector: string = "", mediaQuery: string = ""): string {
-        return Object.keys(style)
-            .filter((key) => Boolean(style[key]))
-            .map((key): string => {
-                const styleKey = key.trim().replace(/[A-Z]|^ms/g, '-$&').toLowerCase();
-                const value = style[key];
-                if (typeof value === "string") {
-                    const cacheKey = styleKey + value + childSelector + mediaQuery;
-                    if (!this.cache[cacheKey]) {
-                        const className = this.prefix + this.rules.length.toString(36);
-                        const style = `.${className}${childSelector}{${styleKey}:${value};}`;
-                        const rule = mediaQuery ? `${mediaQuery}{${style}}` : style;
-                        this.cache[cacheKey] = className;
-                        this.rules.push(rule);
-                        if (this.sheet) {
-                            this.sheet.insertRule(rule, this.sheet.cssRules.length);
-                        }
-                    }
-                    return this.cache[cacheKey];
-                } else {
-                    if (styleKey.startsWith("@media")) {
-                        return this.parseCssObject(value, childSelector, styleKey);
-                    } else {
-                        const formattedChildKey = styleKey.startsWith("&") ? styleKey.substr(1) : " " + styleKey;
-                        return this.parseCssObject(value, childSelector + formattedChildKey, mediaQuery);
-                    }
-                }
-            })
-            .join(" ");
-    }
+	private parseCssObject(
+		style: CssObject,
+		childSelector: string = "",
+		mediaQuery: string = ""
+	): string {
+		return Object.keys(style)
+			.filter((key) => Boolean(style[key]))
+			.map((key): string => {
+				const styleKey = key
+					.trim()
+					.replace(/[A-Z]|^ms/g, "-$&")
+					.toLowerCase();
+				const value = style[key];
+				if (typeof value === "string") {
+					const cacheKey = styleKey + value + childSelector + mediaQuery;
+					if (!this.cache[cacheKey]) {
+						const className = this.prefix + this.rules.length.toString(36);
+						const style = `.${className}${childSelector}{${styleKey}:${value};}`;
+						const rule = mediaQuery ? `${mediaQuery}{${style}}` : style;
+						this.cache[cacheKey] = className;
+						this.rules.push(rule);
+						if (this.sheet) {
+							this.sheet.insertRule(rule, this.sheet.cssRules.length);
+						}
+					}
+					return this.cache[cacheKey];
+				} else {
+					if (styleKey.startsWith("@media")) {
+						return this.parseCssObject(value, childSelector, styleKey);
+					} else {
+						const formattedChildKey = styleKey.startsWith("&")
+							? styleKey.substr(1)
+							: " " + styleKey;
+						return this.parseCssObject(
+							value,
+							childSelector + formattedChildKey,
+							mediaQuery
+						);
+					}
+				}
+			})
+			.join(" ");
+	}
 
+	public getClassNames(style: CssObject) {
+		return this.parseCssObject(style);
+	}
 
-    public getClassNames(style: CssObject) {
-        return this.parseCssObject(style);
-    }
-
-    public getFullCss() {
-        return this.rules.sort().join('\n');
-    }
+	public getFullCss() {
+		return this.rules.sort().join("\n");
+	}
 }
